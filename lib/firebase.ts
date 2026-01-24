932c6bfbf6cf797873708b8a106b54bf2eee5481
cristoferperez <cristoferperez3@gmail.com>
2026-01-24 01:27:14 -0600

    chore: move firebase apiKey to NEXT_PUBLIC_FIREBASE_API_KEY env var

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-DPQ8W3nz2kRTAgi2SWzLpiihK9JwKQ4",
-  authDomain: "emprendedoresgt-ad783.firebaseapp.com",
-  projectId: "emprendedoresgt-ad783",
-  storageBucket: "emprendedoresgt-ad783.firebasestorage.app",
-  messagingSenderId: "652991534707",
-  appId: "1:652991534707:web:205764a5ffcd72d47abc06",
-  measurementId: "G-09T95XCFNP"
+  apiKey: process?.env?.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: 'emprendedoresgt-ad783.firebaseapp.com',
+  projectId: 'emprendedoresgt-ad783',
+  storageBucket: 'emprendedoresgt-ad783.firebasestorage.app',
+  messagingSenderId: '652991534707',
+  appId: '1:652991534707:web:205764a5ffcd72d47abc06',
+  measurementId: 'G-09T95XCFNP'
 };
 
 // Initialize Firebase
